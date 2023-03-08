f5cdf4b43a152dcfb5a2d81241f371330cd1fc69
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:13:45 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1371A | Magical Sticks/180245027.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int t,n;
+ 
+    cin>>t;
+ 
+    while(t--){
+        cin>>n;
+ 
+        n%2==0 ? cout<<n/2<<endl : cout<<(n/2)+1<<endl;
+    }
+}
