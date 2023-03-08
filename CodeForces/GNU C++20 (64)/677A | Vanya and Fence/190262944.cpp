e9776077a7d77812cb0561bb4b4c7749b3f30618
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:10:56 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/677A | Vanya and Fence/190262944.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/677/A
+ 
+int main(){
+    int n,h, c = 0, aux;
+ 
+    cin>>n>>h;
+ 
+    while(n--){
+        cin>>aux;
+ 
+        if(aux>h) c+=2;
+        else c++;
+    }
+ 
+    cout<<c;
+}
