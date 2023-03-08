8ea6609efff2c6ccec70ad7092cfa38febd76050
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:12:52 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1758A | SSeeeeiinngg DDoouubbllee/183815879.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/1758/A
+ 
+int main(){
+    int t;
+    string s, rs;
+ 
+    cin>>t;
+ 
+    while(t--){
+        cin>>s;
+ 
+        rs = string(s.rbegin(),s.rend());
+ 
+        cout<<s+rs<<endl;
+    }
+}
