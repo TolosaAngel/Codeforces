faab48d557aa1588174fccb2f4cf8ea5621e6753
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:11:10 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/935A | Fafa and his Company/189594096.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/935/A
+ 
+int main(){
+    int n,c=0;
+ 
+    cin>>n;
+ 
+    for(int i=1; i<=n/2; i++){
+        if(n%i==0) c++;
+    }
+ 
+    cout<<c;
+}
