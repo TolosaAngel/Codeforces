28db54bde8e44c145cb575de1fd9c28dc1c6aec0
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:10:46 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1790A | Polycarp and the Day of Pi/190778779.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/contest/1790/problem/A
+ 
+int main(){
+    int t;
+    string pi = "31415926535897932384626433832795028841971693993751058";
+ 
+    cin>>t;
+ 
+    while(t--){
+        int c = 0;
+        string n;
+ 
+        cin>>n;
+ 
+        forn(i,n.size()){
+            if(n[i]==pi[i]) c++;
+            else{
+                break;
+            }
+        }
+ 
+        cout<<c<<endl;
+    }
+}
