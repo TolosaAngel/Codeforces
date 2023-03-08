00df44581bb115dd1f3ae07f3d5bb16dbafbcf89
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:14:02 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1525A | Potion-making/178896808.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int gcd(int a, int b){
+     if(b==0) return a;
+     else return gcd(b,a%b);
+}
+ 
+int main(){
+    int t,k;
+ 
+    cin>>t;
+ 
+    while(t--){
+        cin>>k;
+ 
+        cout<<100/gcd(k,100)<<endl;
+    }
+}
