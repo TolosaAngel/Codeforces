8d235397160523c75a475e84477199a9a6926282
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:12:45 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/47A | Triangular numbers/183820609.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/47/A
+ 
+int main(){
+    int n, c = 1, i = 2;
+ 
+    cin>>n;
+ 
+    while (c<n) c+=i, i++;
+ 
+    c==n ? cout<<"YES" : cout<<"NO";
+}
