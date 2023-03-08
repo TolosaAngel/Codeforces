9d4517f975c887f0b95bcf89b48f586055f30fdb
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:11:34 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/791A | Bear and Big Brother/188823894.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/791/A
+ 
+int main(){
+    int a,b,y=0;
+ 
+    cin>>a>>b;
+ 
+    while(a<=b){
+        a *= 3;
+        b *= 2;
+        y++;
+    }
+ 
+    cout<<y;
+}
