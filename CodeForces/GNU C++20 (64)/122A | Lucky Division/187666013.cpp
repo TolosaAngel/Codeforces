ea6a0fb95405bbc1409e109c35849a4b525ff59a
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:12:21 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/122A | Lucky Division/187666013.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/122/A
+ 
+int main(){
+    int n;
+ 
+    cin>>n;
+ 
+    if(n%4==0 || n%7==0 || n%44==0 || n%47==0 || n%74==0 || n%77==0 || n%444==0 || n%447==0 || n%474==0 || n%477==0 || n%744==0 || n%747==0 || n%774==0 || n%777==0) cout<<"YES";
+    else cout<<"NO";
+}
