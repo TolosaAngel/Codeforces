cb3dabbc8acb3e772915fee549a765773c5d6649
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-08 08:12:12 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/466A | Cheap Travel/187863070.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/466/A
+ 
+int main(){
+    int n,m,a,b;
+ 
+    cin>>n>>m>>a>>b;
+ 
+    if(b<a*m){
+        if( (n-(n/m)*m)*a > b ) cout<<(n/m)*b + b;
+        else cout<<(n/m)*b + (n-(n/m)*m)*a;
+    }else cout<<a*n;
+}
