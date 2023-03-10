9ebb2b157265569c71362c41a4f35b2ef2391a7a
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-09 21:25:43 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1722B | Colourblindness/196541107.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/1722/B
+ 
+int main(){
+    int t;
+    cin>>t;
+ 
+    while(t--){
+        int n;
+        string s,s2;
+ 
+        cin>>n>>s>>s2;
+ 
+        forn(i,n){
+            if(s[i]=='B') s[i]='G';
+            if(s2[i]=='B') s2[i]='G';
+        }
+ 
+        if(s==s2) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
