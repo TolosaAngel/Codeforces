9ae230bae564387743ee02b817827a94eaa132ae
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-09 21:27:02 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1772B | Matrix Rotation/188536391.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/problemset/problem/1772/B
+ 
+int main(){
+    int t;
+ 
+    cin>>t;
+ 
+    while(t--){
+        vi v(4);
+ 
+        forn(i,v.size()) cin>>v[i];
+ 
+        int maxi = max_element(v.begin(),v.end()) - v.begin();
+        int mini = min_element(v.begin(),v.end()) - v.begin();
+ 
+        maxi+mini == 3 ? cout<<"YES\n" : cout<<"NO\n";
+    }
+}
