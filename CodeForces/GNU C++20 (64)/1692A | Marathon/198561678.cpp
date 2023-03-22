34702c21174909d4cb3a222a462395f2b368d81a
Angel Tolosa <126740203+TolosaAngel@users.noreply.github.com>
2023-03-21 21:44:43 -0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1692A | Marathon/198561678.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define forsn(i,s,n) for(int i = int(s); i<int(n); i++)
+#define forn(i,n) forsn(i,0,n)
+#define dforsn(i,s,n) for(int i = int(n-1); i>=int(s); i--)
+#define dforn(i,n) dforsn(i,0,n)
+#define DBG(x) cout<<#x<<" = "<<x<<endl;
+#define ioFast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+typedef long long intl;
+typedef vector<int> vi;
+typedef vector<intl> vll;
+ 
+// https://codeforces.com/contest/1692/problem/A
+ 
+int main(){
+    int t;
+    cin>>t;
+ 
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<int(b>a)+int(c>a)+int(d>a)<<endl;
+    }
+}
